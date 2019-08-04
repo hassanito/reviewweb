8522d78aa3b2d5961dcbe50877c82dc601793b6d
hassanito <1Aa_12345>
2019-08-04 17:05:01 +0300

    shop_owner added

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 from PIL import Image
-
+from shops.models import Shop
 # class User(auth.models.User, auth.models.PermissionsMixin):
 #     def __str__(self):
 #         return "@{}".format(self.username)
@@ -25,3 +25,10 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+class ShopOwner(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    shops = models.ForeignKey(Shop,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "@{}".format(self.user.username)
